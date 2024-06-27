49cd24873f20ea6866533344d30c5d90d7854cd8
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-27 11:33:13 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//*****************************************************************************
+//** 58. Length of Last Word leetcode                                        **
+//** Simple but effective approach to count the numbers in a word and save   **
+//** the last word recorded.  It's a slower solution but works.  -Dan        **
+//*****************************************************************************
+
+int lengthOfLastWord(char* s) {
+    int retNum = 0;
+    int convert = 0;
+    int lastword = 0;
+    for(int i = 0; i < strlen(s); i++)
+    {
+        convert = s[i];
+        printf("%d %d %d\n",convert,retNum,lastword);
+        retNum++;
+        if(convert == 32)
+        {
+            if(retNum > 1) lastword = retNum;
+            retNum = 0;
+        }
+    }
+    if(retNum > 0)
+        return retNum;
+    else
+        return lastword-1;
+}
